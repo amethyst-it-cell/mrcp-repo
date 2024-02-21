b0e84425009f0239d506aa4a3010004cac8aee88
mrcpweb-repo <samiullah@secab.org>
2024-02-21 11:21:50 +0530

    title replaced with old

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>sami</title>
+        <title>ii-c</title>
 <!--Changed by samiullah !-->
         <!-- Bootstrap -->
 
